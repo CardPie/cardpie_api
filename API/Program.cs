9a0f104127f49d2dbedaf9cb418ba19a1f14aede
Truong Giang <giangntse150747@gmail.com>
2023-05-13 08:57:04 +0700

    first init

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using AppCore.Configs;
+using AppCore.Extensions;
+using MainData;
+using MainData.Middlewares;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    var connectString = EnvironmentExtension.GetAppConnectionString();
+    options.UseSqlServer(connectString,
+        b =>
+        {
+            b.MigrationsAssembly(Assembly.GetEntryAssembly()?.GetName().Name);
+            b.CommandTimeout(1200);
+        }
+    );
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    options.EnableDetailedErrors();
+}, ServiceLifetime.Transient);
+
+//
+builder.Services.AddControllers();
+builder.Services.AddScoped<MainUnitOfWork>();
+builder.Services.AddConfig(new List<string>
+{
+    "AppCore",
+    "MainData",
+    Assembly.GetEntryAssembly()?.GetName().Name ?? string.Empty
+}, new List<string>
+{
+    nameof(IApiVersionDescriptionProvider),
+    "UnitOfWork",
+    "IUnitOfWork"
+});
+
+//
+
+var app = builder.Build();
+app.UseConfig();
+//
+app.MapControllers();
+app.UseMiddleware<AuthMiddleware>();
+app.Run();
