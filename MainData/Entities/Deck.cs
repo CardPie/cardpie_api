4ab3146af00aa728d392898cfc0b543b72709210
Truong Giang <giangntse150747@gmail.com>
2023-05-20 13:11:11 +0700

    fix conflict

--- MainData/Entities/Deck.cs
+++ MainData/Entities/Deck.cs
@@ -1,5 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using AppCore.Data;
+﻿using AppCore.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -9,10 +9,36 @@ public class Deck : BaseEntity
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Guid UserId { get; set; }
+    public bool IsPublic { get; set; }
+    public DeckColor Color { get; set; }
+    public DeckOrder Order { get; set; }
+    public Guid? UserId { get; set; }
+    
+    public int? RecallStrength { get; set; }
+    public DateTime? ReminderTime { get; set; }
+    public int? LearningLength { get; set; }
+    public SpacedRepetitionStrategy SpacedRepetitionStrategyLevel { get; set; }
+    public Guid FolderId { get; set; }
     
     //RelationShip
     public virtual IEnumerable<FlashCard> FlashCards { get; set; } = new List<FlashCard>();
+    //public virtual IEnumerable<StudySession> StudySessions { get; set; } = new List<StudySession>();
+    public virtual Folder Folder { get; set; } = new Folder();
+}
+
+public enum SpacedRepetitionStrategy
+{
+    Closer = 1, Normal = 2, Distant = 3
+}
+
+public enum DeckColor
+{
+    Purple = 1, Pink = 2, Green = 3, Yellow = 4
+}
+
+public enum DeckOrder
+{
+    Front = 1, Back =2 , Random = 3
 }
 
 public class DeckConfig : IEntityTypeConfiguration<Deck>
@@ -22,6 +48,17 @@ public class DeckConfig : IEntityTypeConfiguration<Deck>
         builder.Property(a => a.UserId).IsRequired().HasDefaultValue(Guid.Empty);
         builder.Property(a => a.Name).IsRequired();
         builder.Property(a => a.Description).IsRequired();
+        builder.Property(a => a.Color).IsRequired().HasDefaultValue(DeckColor.Purple);
+        builder.Property(a => a.Order).IsRequired().HasDefaultValue(DeckOrder.Front);
+        builder.Property(a => a.IsPublic).IsRequired().HasDefaultValue(false);
+        builder.Property(a => a.RecallStrength).IsRequired(false);
+        builder.Property(a => a.ReminderTime).IsRequired(false);
+        builder.Property(a => a.LearningLength).IsRequired(false);
+        builder.Property(a => a.FolderId).IsRequired();
+        builder.Property(a => a.SpacedRepetitionStrategyLevel).IsRequired()
+            .HasDefaultValue(SpacedRepetitionStrategy.Normal);
         builder.HasMany(a => a.FlashCards);
+       // builder.HasMany(a => a.StudySessions);
+        builder.HasOne(a => a.Folder);
     }
 }
---
+++ MainData/Entities/Folder.cs
@@ -0,0 +1,23 @@
+﻿using AppCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MainData.Entities;
+
+public class Folder : BaseEntity
+{
+    public string FolderName { get; set; } = string.Empty;
+    
+    //Relationship
+    public IEnumerable<Deck> Decks { get; set; } = new List<Deck>();
+}
+
+public class FolderConfig : IEntityTypeConfiguration<Folder>
+{
+    public void Configure(EntityTypeBuilder<Folder> builder)
+    {
+        builder.Property(a => a.FolderName).IsRequired();
+        builder.HasMany(a => a.Decks).WithOne(a => a.Folder)
+            .HasForeignKey(x => x.FolderId);
+    }
+}
